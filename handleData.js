4bdd50ab4d80e40dbe4c5b8aa8f0f0275e941b95
nrfactor <nrfactor@gmail.com>
2016-03-04 23:01:37 -0800

    Add prototype playground

--- handleData.js
+++ handleData.js
@@ -1,31 +1,64 @@
-var userData = [];
-/*
-var dataSetSizePrompt = "How many variables do you plan to store?";
-var dataSetSizeDefaultText = "Enter any non-zero and positive integer";
-var dataSetSize = prompt(dataSetSizePrompt, dataSetSizeDefaultText);
-var tempData;
 
-while (isInteger(dataSetSize) && dataSetSize <= 0) {
-    alert("ERROR: Your input must be a non-zero and positive integer.");
-    dataSetSize = prompt(dataSetSizePrompt, dataSetSizeDefaultText);
+function processCharacterData(dataSetSize) {
+	var categories = [];
+	var values = [];
+
+    categories.push(firstData, secondData);
+	
+	return categories;
 }
 
-function isInteger(x) {
-    return (typeof x === 'number') && (x % 1 === 0);
-}
-console.log("Data accepted: " + dataSetSize);
+// Danger zone
 
+var Player = function() {
+	this.isCharacter = true;
+};
 
-for (int i = 0; i < dataSetSize; i++) {
-    tempData = prompt("", "");
-    
-}*/
+Player.prototype.greet = function() {
+	if (this.isCharacter) {
+		console.log("Welcome, " + this.characterName + ", the Level " + this.characterLevel + " " + this.characterClass + " " + this.characterRace);
+	} else {
+		console.log("Welcome, Dungeon Master " + this.playerName);
+	}
+};
 
-// Code above exists from before HTML interface was created
+var Character = function(realName, gameName, level, gameClass, race) {
+	Player.call(this);
+	this.playerName = realName;
+	this.characterName = gameName;
+	this.characterLevel = level;
+	this.characterClass = gameClass;
+	this.characterRace = race;
+};
 
-function processData() {
-    var firstNumber = document.getElementById('firstNum').value;
-    var secondNumber = document.getElementById('secondNum').value;
-    console.log(firstNumber);
-    console.log(secondNumber); 
-}
+Character.prototype = Object.create(Player.prototype);
+Character.prototype.constructor = Character;
+
+var DungeonMaster = function(realName) {
+	this.playerName = realName;
+	this.isCharacter = false;
+};
+
+DungeonMaster.prototype = Object.create(Player.prototype);
+DungeonMaster.prototype.constructor = DungeonMaster;
+
+var Observer = function(realName) {
+	this.playerName = realName;
+};
+
+Observer.prototype = Object.create(Player.prototype);
+Observer.prototype.constructor = Observer;
+
+Observer.prototype.greet = function() {
+	console.log("I, " + this.playerName + " am neither a Character nor a Dungeon Master, and therefore my greeting does not derive from Player.");
+};
+
+var zach = new DungeonMaster("Zach");
+var molly = new Character("Molly", "Kalen", 3, "Rogue", "Human?");
+var nick = new Character("Nick", "Delmirev", 3, "Paladin", "Dragonborn");
+var nate = new Observer("Nate");
+
+zach.greet();
+molly.greet();
+nick.greet();
+nate.greet();
