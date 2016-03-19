a5975a66788f317337fd5879da2827579a5c0cb6
Nick Rebhun <nick@nrebhun.com>
2016-03-19 00:40:48 -0700

    Add MessageThreads and incomplete Player sendMessage function

--- handleData.js
+++ handleData.js
@@ -8,11 +8,42 @@ function processCharacterData(dataSetSize) {
     return categories;
 }
 
-// Begin Message Definition
-var Message = function() {
-    this.timeStamp = 0;
-    this.sender = "Default";
+// Begin MessageThread Definition
+var MessageThread = function(sender, receiver) {
+    this.sender = sender;
+    this.receiver = receiver;
+    this.messages = [];
 }
+
+MessageThread.prototype.addMessageToThread = function (message) {
+    this.content.push(message);
+}
+
+MessageThread.prototype.listMessagesInThread = function () {
+    for (var i = 0; i < this.messages.length; i++) {
+        console.log(this.sender + " -> " + this.receiver);
+        console.log(this.messages[i].getFormattedTimestamp());
+        console.log(this.messages[i].content);
+    }
+}
+
+// Begin Message Definition
+var Message = function(content) {
+    this.timeStamp = new Date();
+    this.content = content;
+}
+
+Message.prototype.getFormattedTimestamp = function () {
+    return  "Message sent on: " + 
+            this.timeStamp.getDate() + "/" +
+            (this.timeStamp.getMonth() + 1) + "/" +
+            this.timeStamp.getFullYear() + " @ " +
+            this.timeStamp.getHours() + ":" +
+            this.timeStamp.getMinutes() + ":" +
+            this.timeStamp.getSeconds();
+}
+
+
 
 // Begin Game Definition
 var Game = function(dungeonMaster, characters) {
@@ -31,6 +62,8 @@ Game.prototype.listPlayers = function() {
 // Begin Player Definition
 var Player = function() {
     this.isCharacter = true;
+    this.addressBook = [];          // Left off here, need each player have knowledge of
+    this.messageThreads = [];       // other players, to send messages.
 };
 
 Player.prototype.greet = function() {
@@ -43,6 +76,29 @@ Player.prototype.greet = function() {
         console.log("Welcome, Dungeon Master " + this.playerName + "!");
     }
 };
+
+Player.prototype.sendMessage = function(recipient, content) {       // Will change all invalid references to "characters" 
+    for (var i = 0; i < characters.length; i++) {                   // into valid references to "this.addressBook"
+        if (characters[i].realName === recipient || characters[i].characterName === recipient) {
+            if (characters[i].messageThreads.length === 0) {
+                var newMessageThread = new MessageThread(this.characterName, recipient);
+                characters[i].messageThreads.push(newMessageThread);
+                characters[i].messageThreads[0].addMessageToThread(new Message(content));
+            } else {
+                for (var j = 0; j < messageThreads.length; j++) {
+                    if (character[i].messageThreads[j].sender === this.characterName) {
+                        character[i].messageThreads[j].addMessageToThread(new Message(content));
+                    } else if (j === (messageThreads.length - 1)) {
+                        var newMessageThread = new MessageThread(this.characterName, recipient, content);
+                        characters[i].messageThreads.push(newMessageThread);
+                    }
+                }
+            }
+        } else {
+            console.log("Character/Player not found.");
+        }
+    }
+}
 
 // Begin Character Definition
 var Character = function(realName, gameName, level, gameClass, race) {
@@ -81,16 +137,17 @@ Observer.prototype.greet = function() {
 };
 
 // Making use of the stuff above
-var game = new Game();
 var zach = new DungeonMaster("Zach");
 var molly = new Character("Molly", "Kalen", 3, "Rogue", "Human?");
 var nick = new Character("Nick", "Delmirev", 3, "Paladin", "Dragonborn");
 var nate = new Observer("Nate");
+var game = new Game(zach, [molly, nick]);
+
+game.listPlayers();
 
 zach.greet();
 molly.greet();
 nick.greet();
 nate.greet();
 
-game.initialize(zach, [molly, nick]);
-game.listPlayers();
+nick.sendMessage("Molly", "Hello, World!");
