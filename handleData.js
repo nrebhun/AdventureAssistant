1c2f8810816cebbaf3f2508c5a9287a8cd5ba007
nrfactor <nrfactor@gmail.com>
2016-04-14 18:27:24 -0700

    Refactor to ES6, update package.json, update .gitignore

--- handleData.js
+++ handleData.js
@@ -9,101 +9,108 @@ function processCharacterData(dataSetSize) {
 }
 
 // Begin Message Definition
-var Message = function(content) {
-    this.timeStamp = new Date();
-    this.content = content;
-}
+class Message {
+    constructor(content) {
+        this.timeStamp = new Date();
+        this.content = content;
+    }
 
-Message.prototype.getFormattedTimestamp = function() {
-    return  "Message sent on: " + 
-            this.timeStamp.getDate() + "/" +
-            (this.timeStamp.getMonth() + 1) + "/" +
-            this.timeStamp.getFullYear() + " @ " +
-            this.timeStamp.getHours() + ":" +
-            this.timeStamp.getMinutes() + ":" +
-            this.timeStamp.getSeconds();
+    getFormattedTimestamp() {
+        return  "Message sent on: " + 
+                this.timeStamp.getDate() + "/" +
+                (this.timeStamp.getMonth() + 1) + "/" +
+                this.timeStamp.getFullYear() + " @ " +
+                this.timeStamp.getHours() + ":" +
+                this.timeStamp.getMinutes() + ":" +
+                this.timeStamp.getSeconds();
+    }
 }
 
 // Begin MessageThread Definition
-var MessageThread = function(sender, receiver) {
-    this.sender = sender;
-    this.receiver = receiver;
-    this.messages = [];
-}
+class MessageThread {
+    constructor(sender, receiver) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.messages = [];
+    }
 
-MessageThread.prototype.addMessageToThread = function(message) {
-    this.messages.push(message);
-}
+    addMessageToThread(message) {
+        this.messages.push(message);
+    }
 
-MessageThread.prototype.listMessagesInThread = function() {
-    console.log(this.sender + " -> " + this.receiver);
-    for (var i = 0; i < this.messages.length; i++) {
-        console.log(this.messages[i].getFormattedTimestamp());
-        console.log(this.messages[i].content);
+    listMessagesInThread() {
+        console.log(this.sender + " <-> " + this.receiver);
+        for (var i = 0; i < this.messages.length; i++) {
+            console.log(this.messages[i].getFormattedTimestamp());
+            console.log(this.messages[i].content);
+        }
     }
 }
 
-// Begin Game Definition
-var Game = function(dungeonMaster, characters) {
-    this.dungeonMaster = dungeonMaster;
-    this.characters = characters;
-}
-
-Game.prototype.listPlayers = function() {
-    console.log("Your Dungeon Master is:\n" + this.dungeonMaster.playerName);
-    console.log("Adventurers:");
-    for (var i = 0; i < this.characters.length; i++) {
-        console.log(this.characters[i].playerName);
+class Game {
+    constructor(dungeonMaster, characters) {
+        this.dungeonMaster = dungeonMaster;
+        this.characters = characters;
     }
-}
 
-Game.prototype.establishAddressBooks = function() {
-    this.dungeonMaster.addressBook = this.dungeonMaster.addressBook.concat(this.characters);
-    for (var i = 0; i < this.characters.length; i++) {
-        this.characters[i].addressBook.push(this.dungeonMaster);
-        this.characters[i].addressBook = this.characters[i].addressBook.concat(this.characters);
-        this.characters[i].addressBook.splice(i+1, 1);
+    listPlayers() {
+        console.log("Your Dungeon Master is:\n" + this.dungeonMaster.playerName);
+        console.log("Adventurers:");
+        for (var i = 0; i < this.characters.length; i++) {
+            console.log(this.characters[i].playerName);
+        }
+    }
+
+    establishAddressBooks() {
+        this.dungeonMaster.addressBook = this.dungeonMaster.addressBook.concat(this.characters);
+        for (var i = 0; i < this.characters.length; i++) {
+            this.characters[i].addressBook.push(this.dungeonMaster);
+            this.characters[i].addressBook = this.characters[i].addressBook.concat(this.characters);
+            this.characters[i].addressBook.splice(i+1, 1);
+        }
     }
 }
 
 // Begin Player Definition
-var Player = function() {
-    this.isCharacter = true;
-    this.addressBook = [];
-    this.messageThreads = [];
-};
-
-Player.prototype.greet = function() {
-    if (this.isCharacter) {
-        console.log("Welcome, " + this.characterName + 
-                    ", the Level " + this.characterLevel + 
-                    " " + this.characterClass + 
-                    " " + this.characterRace + "!");
-    } else {
-        console.log("Welcome, Dungeon Master " + this.playerName + "!");
+class Player {
+    constructor() {
+        this.isCharacter = true;
+        this.addressBook = [];
+        this.messageThreads = [];
     }
-};
 
-Player.prototype.printAddressBook = function() {
-    for (var i = 0; i < this.addressBook.length; i++) {
-        console.log(this.addressBook[i]);
+    greet() {
+        if (this.isCharacter) {
+            console.log("Welcome, " + this.characterName + 
+                        ", the Level " + this.characterLevel + 
+                        " " + this.characterClass + 
+                        " " + this.characterRace + "!");
+        } else {
+            console.log("Welcome, Dungeon Master " + this.playerName + "!");
+        }
     }
-}
 
-Player.prototype.sendMessage = function(recipient, content) {
-    for (var i = 0; i < this.addressBook.length; i++) {
-        if (this.addressBook[i].playerName === recipient || this.addressBook[i].characterName === recipient) {
-            if (this.addressBook[i].messageThreads.length === 0) {
-                var newMessageThread = new MessageThread(this.characterName, this.addressBook[i].characterName);
-                this.addressBook[i].messageThreads.push(newMessageThread);
-                this.addressBook[i].messageThreads[0].addMessageToThread(new Message(content));
-            } else {
-                for (var j = 0; j < this.addressBook[i].messageThreads.length; j++) {
-                    if (this.addressBook[i].messageThreads[j].sender === this.characterName) {
-                        this.addressBook[i].messageThreads[j].addMessageToThread(new Message(content));
-                    } else if (j === (messageThreads.length - 1)) {
-                        var newMessageThread = new MessageThread(this.characterName, recipient, content);
-                        this.addressBook[i].messageThreads.push(newMessageThread);
+    printAddressBook() {
+        for (var i = 0; i < this.addressBook.length; i++) {
+            console.log(this.addressBook[i]);
+        }
+    }
+
+    sendMessage(recipient, content) {
+        for (var i = 0; i < this.addressBook.length; i++) {
+            if (this.addressBook[i].playerName === recipient || this.addressBook[i].characterName === recipient) {
+                if (this.addressBook[i].messageThreads.length === 0) {
+                    var newMessageThread = new MessageThread(this.characterName, this.addressBook[i].characterName);
+                    this.addressBook[i].messageThreads.push(newMessageThread);
+                    this.addressBook[i].messageThreads[0].addMessageToThread(new Message(content));
+                } else {
+                    for (var j = 0; j < this.addressBook[i].messageThreads.length; j++) {
+                        if (this.addressBook[i].messageThreads[j].sender === this.characterName) {
+                            this.addressBook[i].messageThreads[j].addMessageToThread(new Message(content));
+                        } else if (j === (messageThreads.length - 1)) {
+                            var newMessageThread = new MessageThread(this.characterName, recipient, content);
+                            this.addressBook[i].messageThreads.push(newMessageThread);
+                        }
                     }
                 }
             }
@@ -112,48 +119,33 @@ Player.prototype.sendMessage = function(recipient, content) {
 }
 
 // Begin Character Definition
-var Character = function(realName, gameName, level, gameClass, race) {
-    Player.call(this); 
-    this.playerName = realName;
-    this.characterName = gameName;
-    this.characterLevel = level;
-    this.characterClass = gameClass;
-    this.characterRace = race;
-};
+class Character extends Player {
+    constructor() {
+        super();
 
-Character.prototype = Object.create(Player.prototype);
-Character.prototype.constructor = Character;
+        this.playerName = realName;
+        this.characterName = gameName;
+        this.characterLevel = level;
+        this.characterClass = gameClass;
+        this.characterRace = race;
+    }
+}
 
 // Begin Dungeon Master Definition
-var DungeonMaster = function(realName) {
-    Player.call(this);
-    this.playerName = realName;
-    this.characterName = "Dungeon Master";
-    this.isCharacter = false;
-};
+class DungeonMaster {
+    constructor() {
+        super();
 
-DungeonMaster.prototype = Object.create(Player.prototype);
-DungeonMaster.prototype.constructor = DungeonMaster;
-
-// Begin Observer Definition
-var Observer = function(realName) {
-    this.playerName = realName;
-};
-
-Observer.prototype = Object.create(Player.prototype);
-Observer.prototype.constructor = Observer;
-
-Observer.prototype.greet = function() {
-    console.log("I, " + this.playerName + " am neither a Character nor " +
-                "a Dungeon Master, and therefore my greeting should be " +
-                "different from a Player.");
-};
+        this.playerName = realName;
+        this.characterName = "Dungeon Master";
+        this.isCharacter = false;
+    }
+}
 
 // Making use of the stuff above
 var zach = new DungeonMaster("Zach");
 var molly = new Character("Molly", "Kalen", 3, "Rogue", "Human?");
 var nick = new Character("Nick", "Delmirev", 3, "Paladin", "Dragonborn");
-var nate = new Observer("Nate");
 var game = new Game(zach, [molly, nick]);
 game.establishAddressBooks();
 
